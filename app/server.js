39675dc3b61b9ef80dfb2610a68ce44508ddbf52
hoangvietcoder <maihoangviet4869@gmail.com>
2022-11-18 11:25:27 +0700

    update v3

--- app/server.js
+++ app/server.js
@@ -15,22 +15,22 @@ app.get('/', function (req, res) {
 });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "images/profile.jpg"));
   res.writeHead(200, { 'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// use when starting application locally
+// Use when starting application locally
 let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
-// use when starting application as docker container
+// Use when starting application as docker container
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+// Pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "user-account";
+let databaseName = "my-db";
 
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
@@ -50,12 +50,14 @@ app.post('/update-profile', function (req, res) {
     });
 
   });
+
   // Send response
   res.send(userObj);
 });
 
 app.get('/get-profile', function (req, res) {
   let response = {};
+
   // Connect to the db
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
@@ -76,5 +78,5 @@ app.get('/get-profile', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+  console.log("App listening on port 3000!");
 });
